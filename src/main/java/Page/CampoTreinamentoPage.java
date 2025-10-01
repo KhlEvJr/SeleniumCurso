d1e6b76fbffc217575c426ec9f2c34c2b9bb5878
Evandro Junior <khaljc22@proton.me>
2025-10-01 09:27:30 -0300

    69- Herança e comportamento ( Falta resolver conflitos de dependencia)

--- src/main/java/Page/CampoTreinamentoPage.java
+++ src/main/java/Page/CampoTreinamentoPage.java
@@ -1,3 +1,5 @@
+package Page;
+
 import SeleniumCursoCORE.DSL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
--- src/main/java/Suites/SuiteTeste.java
+++ src/main/java/Suites/SuiteTeste.java
@@ -1,13 +1,20 @@
+package Suites;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import test.java.TesteCadastro;
+import test.java.TesteRegrasCadastro;
+import test.java.TesteRegrasCadastroastro;
+import test.java.TesteCampoTreinamento;
 
 @RunWith(Suite.class)
 @SuiteClasses({
         TesteCadastro.class,
         TesteRegrasCadastro.class,
+        TesteRegrasCadastroastro.class,
         TesteCampoTreinamento.class
 })
 public class SuiteTeste {
-
 }
